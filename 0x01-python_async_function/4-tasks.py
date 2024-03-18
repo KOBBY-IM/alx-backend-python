4595822ade33c37ba7031d0f69d88fa536bdf016
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-18 22:26:43 +0000

    fixed import task 4from 3-tasks import

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Tasks"""
 
+from ast import List
 import asyncio
 from asyncio import Task
-from random import randint
+from 3-tasks import task_wait_random
+
+task_wait_name = __import__('3-tasks').task_wait_random
 
 
-async def wait_random(max_delay: int) -> float:
-    """Asynchronous routine that waits for a random delay between 0
-    and max_delay (included)and returns it.
-    """
-    delay = randint(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    delays = []
+    for _ in range(n):
+        delay = await task_wait_random(max_delay)
+        delays.append(delay)
+    return delays
