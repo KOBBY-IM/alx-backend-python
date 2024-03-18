57ddc0c539b15c61473758e0f4a8eee33322cb89
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-18 21:26:56 +0000

    fixed requirement

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,9 +2,10 @@
 """Concurrent coroutines"""
 
 import asyncio
-import typing
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: float) -> typing.List[float]:
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,9 +5,10 @@ import asyncio
 from asyncio import Task
 from random import randint
 
+
 async def wait_random(max_delay: int) -> float:
-    """Asynchronous routine that waits for a random delay between 0 and max_delay (included)
-    and returns it.
+    """Asynchronous routine that waits for a random delay between 0
+    and max_delay (included)and returns it.
     """
     delay = randint(0, max_delay)
     await asyncio.sleep(delay)
